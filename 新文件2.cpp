ae61c28b0faf6ad1ad119ea22eee096f40533ea3
minmin <minmin19920116@gmail.com>
2015-03-11 11:10:13 +0800

    first step

---
+++ 新文件2.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include<stdlib.h>
+#include<math.h>
+
+int main(int argc,char*argv[]){
+    char name[50]="Yu-Hsun Lee";
+	printf("hello world\n");
+	printf("%d\n",2+3);
+	printf("%f\n",1.2+34.654);
+	printf("%f\n",cos(2.0*M_PI));
+	printf("%s\n",name);
+	return 123;
+}
